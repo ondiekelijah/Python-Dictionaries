0605acedb0939b0e08d43789eb5ff895f8d999c4
ONDIEK ELIJAH OCHIENG <57575260+Dev-Elie@users.noreply.github.com>
2021-08-07 10:52:17 +0300

    Add files via upload

---
+++ histogram.py
@@ -0,0 +1,15 @@
+alphabet = "abcdefghijklmnopqrstuvwxyz"   
+
+test_dups = ["zzz","dog","bookkeeper","subdermatoglyphic","subdermatoglyphics"] 
+
+test_miss = ["zzz","subdermatoglyphic","the quick brown fox jumps over the lazy dog"] 
+
+def histogram(s):
+     d = dict()
+     for c in s:
+          if c not in d:
+               d[c] = 1
+          else:
+               d[c] += 1
+     return d 
+
